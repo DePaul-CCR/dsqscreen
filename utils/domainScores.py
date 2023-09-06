89cbaf39f01095c3d8e358b722a9db8ff508a206
mooserson <pmcathey@gmail.com>
2023-09-06 11:31:53 -0500

    reorganizes domains to match CCC domains

--- utils/domainScores.py
+++ utils/domainScores.py
@@ -4,92 +4,63 @@ Created on Wed Feb 22 14:10:11 2023
 
 @author: sbenner
 
-DSQ items grouped in domains per https://www.researchgate.net/publication/358982902_Evaluating_case_diagnostic_criteria_for_Myalgic_EncephalomyelitisChronic_Fatigue_Syndrome_MECFS_Toward_an_empirical_case_definition
--K.E. Conroy et al. 2022
+DSQ items grouped mostly according to CCC domains
 """
 
 import pandas as pd
 import numpy as np
 
-#import plotly.offline as pyo
+pem_domain = ['heavy14f', 'soreness15f', 'mental16f', 'minimum17f', 'drained18f',
+              'heavy14s', 'soreness15s', 'mental16s', 'minimum17s', 'drained18s']
 
-pemdomain = ['minimum17f', 'soreness15f', 'drained18f', 'mental16f', 'heavy14f', 'weakness33f',
-             'minimum17s', 'soreness15s', 'drained18s', 'mental16s', 'heavy14s', 'weakness33s']
+sleep_domain = ['unrefreshed19f', 'nap20f', 'falling21f', 'staying22f', 'early23f', 'allday24f',
+                'unrefreshed19s', 'nap20s', 'falling21s', 'staying22s', 'early23s', 'allday24s']
 
+pain_domain = ['musclepain25f', 'jointpain26f', 'eyepain27f', 'chestpain28f', 'bloating29f', 'stomach30f', 'headaches31s',
+               'musclepain25s', 'jointpain26s', 'eyepain27s', 'chestpain28s', 'bloating29s', 'stomach30s', 'headaches31f']
 
-sleepdomain = ['unrefreshed19f', 'nap20f', 'falling21f', 'staying22f', 'early23f', 'allday24f',
-               'unrefreshed19s', 'nap20s', 'falling21s', 'staying22s', 'early23s', 'allday24s']
+cog_domain = ['twitches32f', 'weakness33f', 'noise34f', 'lights35f', 'remember36f', 'difficulty37f', 'word38f', 'understanding39f', 'focus40f', 'unable41f', 'depth42f', 'slowness43f', 'absent44f',
+              'twitches32s', 'weakness33s', 'noise34s', 'lights35s', 'remember36s', 'difficulty37s', 'word38s', 'understanding39s', 'focus40s', 'unable41s', 'depth42s', 'slowness43s', 'absent44s']
 
-cogdomain = ['remember36f', 'difficulty37f', 'word38f', 'absent44f', 'focus40f','slowness43f','understanding39f', 'unable41f',
-             'remember36s', 'difficulty37s', 'word38s', 'absent44s', 'focus40s','slowness43s','understanding39s', 'unable41s']
+autonomic_domain = ['bladder45f', 'bowel46f', 'nausea47f', 'unsteady48f', 'shortness49f', 'dizz50f', 'irregular51f', 
+                    'bladder45s', 'bowel46s', 'nausea47s', 'unsteady48s', 'shortness49s', 'dizz50s', 'irregular51s']
 
-paindomain = ['musclepain25f', 'jointpain26f', 'eyepain27f', 'headaches31s',
-              'musclepain25s', 'jointpain26s', 'eyepain27s', 'headaches31f']
+neuro_domain = ['weight52f', 'appetite53f', 'sweating54f', 'night55f', 'limbs56f', 'chills57f', 'hot58f', 'htemp59f', 'ltemp60f', 'alcohol61f', 
+                'weight52s', 'appetite53s', 'sweating54s', 'night55s', 'limbs56s', 'chills57s', 'hot58s', 'htemp59s', 'ltemp60s', 'alcohol61s']
 
-
-gastrodomain = ['bloating29f', 'bowel46f', 'stomach30f', 'bladder45f',
-                'bloating29s', 'bowel46s', 'stomach30s', 'bladder45s']
-
-orthodomain = ['unsteady48f', 'chestpain28f', 'shortness49f', 'dizz50f', 'irregular51f', 'nausea47f',
-               'unsteady48s', 'chestpain28s', 'shortness49s', 'dizz50s', 'irregular51s', 'nausea47s']
-
-circdomain = ['limbs56f', 'hot58f', 'ltemp60f', 'sweating54f', 'chills57f', 'weight52f', 'appetite53f', 'night55f',
-              'limbs56s', 'hot58s', 'ltemp60s', 'sweating54s', 'chills57s', 'weight52s', 'appetite53s', 'night55s']
-
-immunedomain = ['flu65f', 'fever64f', 'lymphnodes63f', 'sorethroat62f', 'htemp59f',
-                'flu65s', 'fever64s', 'lymphnodes63s', 'sorethroat62s', 'htemp59s']
-
-neuroendomain = ['smells66f', 'alcohol61f', 'twitches32f', 'noise34f', 'lights35f', 'depth42f',
-         'smells66s', 'alcohol61s', 'twitches32s', 'noise34s', 'lights35s', 'depth42s']
+immune_domain = ['sorethroat62f', 'lymphnodes63f', 'fever64f', 'flu65f', 'smells66f', 
+                'sorethroat62s', 'lymphnodes63s', 'fever64s', 'flu65s', 'smells66s']
 
 # domains for the short form:
-sf_pemdomain = ['minimum17f', 'soreness15f', 'minimum17s', 'soreness15s']
-
-
-sf_sleepdomain = ['unrefreshed19f', 'unrefreshed19s']
-
-sf_cogdomain = ['remember36f', 'difficulty37f', 'remember36s', 'difficulty37s']
-
-sf_paindomain = ['musclepain25f', 'musclepain25s']
-
-
-sf_gastrodomain = ['bloating29f', 'bowel46f', 'bloating29s', 'bowel46s']
-
-sf_orthodomain = ['unsteady48f', 'unsteady48s']
-
-sf_circdomain = ['limbs56f', 'hot58f', 'limbs56s', 'hot58s']
-
-sf_immunedomain = ['flu65f','flu65s']
-
-sf_neuroendomain = ['smells66f', 'smells66s']
+sf_pem_domain = ['soreness15f', 'soreness15s', 'minimum17f', 'minimum17s']
+sf_sleep_domain = ['unrefreshed19f', 'unrefreshed19s']
+sf_pain_domain = ['musclepain25f', 'musclepain25s', 'bloating29f', 'bloating29s']
+sf_cog_domain = ['remember36f', 'remember36s', 'difficulty37f', 'difficulty37s']
+sf_autonomic_domain = ['bowel46f', 'bowel46s', 'unsteady48f', 'unsteady48s']
+sf_neuro_domain = ['limbs56f', 'limbs56s', 'hot58f', 'hot58s']
+sf_immune_domain = ['flu65f', 'flu65s', 'smells66f', 'smells66s']
 
 #Maybe change this to the imputed data:
 df = pd.read_csv('utils/MECFS and Controls F+S Reduction.csv')
-
 sdf = df
 
-df['fatigue'] = np.mean(df[['fatigue13f', 'fatigue13s']], axis=1)
-df['pemmean'] = np.mean(df[pemdomain], axis=1)
-df['sleepmean'] = np.mean(df[sleepdomain], axis=1)
-df['cogmean'] = np.mean(df[cogdomain], axis=1)
-df['painmean'] = np.mean(df[paindomain], axis=1)
-df['gastromean'] = np.mean(df[gastrodomain], axis=1)
-df['orthomean'] = np.mean(df[orthodomain], axis=1)
-df['circmean'] = np.mean(df[circdomain], axis=1)
-df['immunemean'] = np.mean(df[immunedomain], axis=1)
-df['neuroendomain'] = np.mean(df[neuroendomain], axis=1)
+df['fatigue_mean'] = np.mean(df[['fatigue13f', 'fatigue13s']], axis=1)
+df['pem_mean'] = np.mean(df[pem_domain], axis=1)
+df['sleep_mean'] = np.mean(df[sleep_domain], axis=1)
+df['pain_mean'] = np.mean(df[pain_domain], axis=1)
+df['cog_mean'] = np.mean(df[cog_domain], axis=1)
+df['auto_mean'] = np.mean(df[autonomic_domain], axis=1)
+df['neuro_mean'] = np.mean(df[neuro_domain], axis=1)
+df['immune_mean'] = np.mean(df[immune_domain], axis=1)
 
-
-sdf['fatigue'] = np.mean(sdf[['fatigue13f', 'fatigue13s']], axis=1)
-sdf['pemmean'] = np.mean(sdf[sf_pemdomain], axis=1)
-sdf['sleepmean'] = np.mean(sdf[sf_sleepdomain], axis=1)
-sdf['cogmean'] = np.mean(sdf[sf_cogdomain], axis=1)
-sdf['painmean'] = np.mean(sdf[sf_paindomain], axis=1)
-sdf['gastromean'] = np.mean(sdf[sf_gastrodomain], axis=1)
-sdf['orthomean'] = np.mean(sdf[sf_orthodomain], axis=1)
-sdf['circmean'] = np.mean(sdf[sf_circdomain], axis=1)
-sdf['immunemean'] = np.mean(sdf[sf_immunedomain], axis=1)
-sdf['neuroendomain'] = np.mean(sdf[sf_neuroendomain], axis=1)
+sdf['fatigue_mean'] = np.mean(sdf[['fatigue13f', 'fatigue13s']], axis=1)
+sdf['pem_mean'] = np.mean(sdf[sf_pem_domain], axis=1)
+sdf['sleep_mean'] = np.mean(sdf[sf_sleep_domain], axis=1)
+sdf['pain_mean'] = np.mean(sdf[sf_pain_domain], axis=1)
+sdf['cog_mean'] = np.mean(sdf[sf_cog_domain], axis=1)
+sdf['auto_mean'] = np.mean(sdf[sf_autonomic_domain], axis=1)
+sdf['neuro_mean'] = np.mean(sdf[sf_neuro_domain], axis=1)
+sdf['immune_mean'] = np.mean(sdf[sf_immune_domain], axis=1)
 
 
 '''
