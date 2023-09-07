01d6bf448d86ae478a6b54927637a3b43fb3ad42
mooserson <pmcathey@gmail.com>
2023-09-07 09:57:32 -0500

    refactors chart function and switches domains to CCC domains

--- utils/short_form_utils.py
+++ utils/short_form_utils.py
@@ -8,22 +8,7 @@ from utils.general_utils import get_score
 # see dsqitems_and_routes_map.txt for info on each section of the screener
 
 def short_form_diagnose():
-    # we use CCC domains / DSQSF scoring for placing items in this bar chart
-    fatigue_score = (get_score("fatiguescoref") + get_score("fatiguescores")) / 2
-    pem_score = (get_score("minexf") + get_score("minexs") + get_score('soref') + get_score('sores')) / 4
-    sleep_score = (get_score("sleepf") + get_score("sleeps")) / 2
-    pain_score = (get_score('musclef') + get_score('muscles') + get_score('bloatf') + get_score('bloats')) / 4
-    cog_score = (get_score("rememberf") + get_score("remembers") + get_score('attentionf') + get_score('attentions')) / 4
-    autonomic_score = (get_score('unsteadyf') + get_score('unsteadys') + get_score('bowelf') + get_score('bowels')) / 4
-    neuro_score = (get_score('limbsf') + get_score('limbss') + get_score('hotf') + get_score('hots')) / 4
-    immune_score = (get_score('fluf') + get_score('flus') + get_score('smellf') + get_score('smells')) / 4
-
-    user_scores = [fatigue_score, pem_score, sleep_score, cog_score, pain_score, autonomic_score, neuro_score, immune_score]
-
-    df = ds.sdf
-    mecfs = df[(df['dx'] == 1)]
-    # takes the symptom domain score means calculated in domainScores.py
-    cfsdomains = np.mean(mecfs.iloc[:, 110:117], axis=0)
+    graphJSON = short_form_chart()
 
     #Assesses the IOM Criteria
     iomfatiguecheck = "No"
@@ -94,7 +79,8 @@ def short_form_diagnose():
         ccc_cog = 0
         ccc_cogcheck = "No"
 
-    if get_score('musclef') >= 2 and get_score('muscles') >= 2:
+    if get_score('musclef') >= 2 and get_score('muscles') >= 2 or (
+       get_score('bloatf') >= 2 and get_score('bloats') >= 2):
         ccc_pain = 1
         ccc_paincheck = "Yes"
     else:
@@ -102,8 +88,7 @@ def short_form_diagnose():
         ccc_paincheck = "No"
     
     if (get_score('unsteadyf') >= 2 and get_score('unsteadys') >= 2) or (
-        get_score('bowelf') >= 2 and get_score('bowels') >= 2) or (
-        get_score('bloatf') >= 2 and get_score('bloats') >= 2):
+        get_score('bowelf') >= 2 and get_score('bowels') >= 2):
         ccc_auto = 1
         ccc_autocheck = "Yes"
     else:
@@ -134,7 +119,7 @@ def short_form_diagnose():
         ccc_sleepcheck = "No"
     
     ccc_poly = np.sum([ccc_auto, ccc_neuro, ccc_immune])
-    # most of the symptoms are required, but there is one polythetic criteria, shown here by ccc_poly
+    # most of the domains are required, but there is one polythetic criteria, shown here by ccc_poly
     if np.sum([ccc_fatigue, ccc_reduction, ccc_pem, ccc_sleep, ccc_pain, ccc_cog]) >= 6 and ccc_poly >= 2:
         ccc_dx = "Met"
         ccc_msg = "Your responses suggest that you meet the Canadian Consensus Criteria for ME/CFS. " \
@@ -143,14 +128,6 @@ def short_form_diagnose():
         ccc_dx = "Not met"
         ccc_msg = "Your responses do not meet the Canadian Consensus Criteria for ME/CFS."
 
-    # categories = [*feature_list, feature_list[0]]
-    categories = ['Fatigue', 'PEM', 'Sleep', 'Cognitive Impairment', 'Pain', 'Gastro Problems',
-                  'Orthostatic Intolerance', 'Circulatory Problems', 'Immune System', 'Neuroendocrine Problems']
-
-    # converts scores to 100pt scale
-    user_scores = np.multiply(user_scores, 25).tolist()
-    cfsdomains = np.multiply(cfsdomains, 25).tolist()
-
     # diagnostic message ccc OR iom
     dx_met = False
     if ccc_dx == "Met" or iomdxcheck == "Met":
@@ -158,6 +135,39 @@ def short_form_diagnose():
         dx_met = True
     else:
         short_form_message = "Based on your responses it does not appear you have MECFS."
+
+    return render_template("results/graph2.html", graphJSON=graphJSON, ccc_msg=ccc_msg, ccc_fatiguecheck=ccc_fatiguecheck,
+                           ccc_pemcheck=ccc_pemcheck, ccc_paincheck=ccc_paincheck, ccc_sleepcheck=ccc_sleepcheck,
+                           ccc_cogcheck=ccc_cogcheck, ccc_autocheck=ccc_autocheck, ccc_immunecheck=ccc_immunecheck,
+                           ccc_neurocheck=ccc_neurocheck, ccc_dx=ccc_dx, ccc_reductioncheck=ccc_reductioncheck, ccc_poly=ccc_poly,
+                           iomfatiguecheck=iomfatiguecheck, iomreductioncheck=iomreductioncheck,
+                           iompemcheck=iompemcheck, iomdxcheck=iomdxcheck, iom_msg=iom_msg,
+                           iomsleepcheck=iomsleepcheck, iomcogcheck=iomcogcheck, iomorthocheck=iomorthocheck,
+                           short_form_message=short_form_message,dx_met=dx_met
+                           )
+
+def short_form_chart():
+    # we use CCC domains / DSQSF scoring for placing items in this bar chart
+    fatigue_score = (get_score("fatiguescoref") + get_score("fatiguescores")) / 2
+    pem_score = (get_score("minexf") + get_score("minexs") + get_score('soref') + get_score('sores')) / 4
+    sleep_score = (get_score("sleepf") + get_score("sleeps")) / 2
+    pain_score = (get_score('musclef') + get_score('muscles') + get_score('bloatf') + get_score('bloats')) / 4
+    cog_score = (get_score("rememberf") + get_score("remembers") + get_score('attentionf') + get_score('attentions')) / 4
+    autonomic_score = (get_score('unsteadyf') + get_score('unsteadys') + get_score('bowelf') + get_score('bowels')) / 4
+    neuro_score = (get_score('limbsf') + get_score('limbss') + get_score('hotf') + get_score('hots')) / 4
+    immune_score = (get_score('fluf') + get_score('flus') + get_score('smellf') + get_score('smells')) / 4
+
+    user_scores = [fatigue_score, pem_score, sleep_score, pain_score, cog_score, autonomic_score, neuro_score, immune_score]
+    categories = ['Fatigue', 'PEM', 'Sleep Issues', 'Pain', 'Cognitive Problems', 'Autonomic Problems', 'Neurological Problems', 'Immune System']
+
+    df = ds.sdf
+    mecfs = df[(df['dx'] == 1)]
+    # takes the symptom domain score means calculated in domainScores.py
+    cfsdomains = np.mean(mecfs.iloc[:, 110:118], axis=0)
+
+    # converts scores to 100pt scale for chart
+    user_scores = np.multiply(user_scores, 25).tolist()
+    cfsdomains = np.multiply(cfsdomains, 25).tolist()
 
     # Creates a figure using the plotly library, which can be dynamically embedded in the HTML page
     fig = go.Figure(
@@ -177,14 +187,4 @@ def short_form_diagnose():
                       xaxis_title='Symptom Domains')
     fig.update_yaxes(range=[0, 100], dtick=25)
     # This converts to figure fig to a JSON object so it can be dynamically rendered with javascript on the page
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return render_template("results/graph2.html", graphJSON=graphJSON, ccc_msg=ccc_msg, ccc_fatiguecheck=ccc_fatiguecheck,
-                           ccc_pemcheck=ccc_pemcheck, ccc_paincheck=ccc_paincheck, ccc_sleepcheck=ccc_sleepcheck,
-                           ccc_cogcheck=ccc_cogcheck, ccc_autocheck=ccc_autocheck, ccc_immunecheck=ccc_immunecheck,
-                           ccc_neurocheck=ccc_neurocheck, ccc_dx=ccc_dx, ccc_reductioncheck=ccc_reductioncheck, ccc_poly=ccc_poly,
-                           iomfatiguecheck=iomfatiguecheck, iomreductioncheck=iomreductioncheck,
-                           iompemcheck=iompemcheck, iomdxcheck=iomdxcheck, iom_msg=iom_msg,
-                           iomsleepcheck=iomsleepcheck, iomcogcheck=iomcogcheck, iomorthocheck=iomorthocheck,
-                           short_form_message=short_form_message,dx_met=dx_met
-                           )
+    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
