33fe5931ad25602c17d2e2bc95d4f353a617c312
mooserson <pmcathey@gmail.com>
2023-11-29 14:15:39 -0600

    debugger 1

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -5,6 +5,10 @@ def get_score(item_name):
     return int(session[item_name])
 
 def get_client_ip():
+    print("--------------------------------------------------------------------------------------------------------")
+    print(request.environ)
+    print("--------------------------------------------------------------------------------------------------------")
+    print(request)
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         return request.environ['REMOTE_ADDR']
     else:
